ef8be526c28b706a1905136a8a65c04c578883e1
Gabriel Ng <gabrielng15@gmail.com>
2018-08-25 13:48:24 -0700

    #16 created Store file and reducer index

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 
+import ConfigureStore from "../redux/configStore";
 import './styles/index.css';
-
 import App from './App';
 
 import registerServiceWorker from './registerServiceWorker';
 
+const store = ConfigureStore();
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
