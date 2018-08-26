e2d6aabb73cc61b38b0535272ee377f97e892a12
Gabriel Ng <gabrielng15@gmail.com>
2018-08-26 13:21:53 -0700

    #18 created more components in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
+import Navbar from './components/navbarComponents/Navbar';
+
 class App extends Component {
   render() {
     return (
       <div>
+        <Navbar />
       </div>
     );
   }
