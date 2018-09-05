a12d6f04a3c8940ffb09542db1e62d10b4450336
Gabriel Ng <gabrielng15@gmail.com>
2018-09-05 15:37:14 -0700

    #77 added file upload check to upload controller

--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 // Set Storage Engine
 const storage = multer.diskStorage({
-    destination: '../public/uploads',
+    destination: 'public/uploads',
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
@@ -11,12 +11,33 @@ const storage = multer.diskStorage({
 
 // init Upload
 const upload = multer({
-    storage: storage
-}).single("myImage"); //this is the form name!!! CHANGE WHEN MAKE FORM IF NEEDED
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        checkFileType(file, cb);
+    }
+}).single("myImage"); 
+//this is the form name!!! CHANGE WHEN MAKE FORM IF NEEDED
+
+// file upload check
+const checkFileType = (file, cb) => {
+    // allowed file tpyes
+    const filetypes = /jpeg|jpg|gif|png/;
+    // check file uplaod type
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // check mimetype
+    const mimetype = filetypes.test(file.mimetype);
+    
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('Error: Images Only.')
+    }
+}
 
 // POST /api/:user_id/upload
 const uploadImage = (req, res) => {
     // console.log("uploadImage", req);
+    console.log("in route");
     
     upload(req, res, (err) => {
         // console.log("upload", req);
