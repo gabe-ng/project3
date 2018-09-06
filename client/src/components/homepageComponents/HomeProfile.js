6245d4320285a365c2321aa0c9b32d6b58d6f396
Gabriel Ng <gabrielng15@gmail.com>
2018-09-06 11:54:49 -0700

    #85 added profile picture to homepage

--- client/src/components/homepageComponents/HomeProfile.js
+++ client/src/components/homepageComponents/HomeProfile.js
@@ -1,23 +1,64 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+import { getProfileImage } from "../../redux/actions/profileImageActions";
+
 
 class HomeProfile extends Component {
-  render() {
-   
-    let profile;
+  componentDidMount = () => {
+    console.log("mounting HOME");
+  };
 
-    if (this.props.currentUser.user) {
-      profile = 
-      (<div>
-        <h2>Hi {this.props.currentUser.user.name}!</h2>
-      </div>)
+  componentWillUpdate = (nextProps, nextState) => {
+    console.log("IN CWUUUUU");
+    
+    if (nextProps.currentUser.user !== this.props.currentUser.user) {
+      this.props
+        .getProfileImage(nextProps.currentUser.user.id)
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => console.log(err));
+    }
+  };
+
+  render() {
+    console.log("JQJQJDQJDDJQ", this.props.currentUser.user);
+    console.log(this.props);
+    
+
+    let profile;
+    let image;
+
+    if (this.props.imageState.image) {
+      image = <img src={`http://localhost:3001/profileimage/${
+        this.props.imageState.image[0].name
+        }`} className="homepage-image" />
     }
 
-    return (
-      <div className="homepage-profile">
-       {profile}
-      </div>
-    );
+    if (this.props.currentUser.user) {
+      profile = (
+        <div className="homepage-profile">
+          <h2>Hi {this.props.currentUser.user.name}!</h2>
+          {image}
+        </div>
+      );
+    }
+
+    return <div>{profile}</div>;
   }
 }
 
-export default HomeProfile;
+const mapStateToProps = state => {
+  return {
+    imageState: state.profileImageReducer
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ getProfileImage }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeProfile);
+
